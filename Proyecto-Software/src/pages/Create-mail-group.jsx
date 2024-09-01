84f06dbef0f3a34f4f2d9f37e3da7138be16bde3
Loglady13 <raschelljq0203@estudiantec.cr>
2024-09-01 14:44:19 -0600

    Update Create-mail-group.jsx

--- Proyecto-Software/src/pages/Create-mail-group.jsx
+++ Proyecto-Software/src/pages/Create-mail-group.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 import SidebarAdmin from '../shared-components/Sidebar-admin';
+import appFirebase from '../services/credenciales';
+import {getAuth, signAuth} from 'firebase/auth';
+
 
 const CreateMailGroup=()=>{
     return(
-        <div><SidebarAdmin/></div>
+        <div>
+        <SidebarAdmin/>
+        <div className='container'>
+            <p>Bienvenido</p>
+        </div>
+        </div>
+
+
+
     );
 
 }
